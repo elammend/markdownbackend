09460b89770209f8c2b4eefd01572575ce833a61
elammend <eddy.lam97@gmail.com>
2020-07-02 09:48:38 -0700

    corrected cors origin

--- app.js
+++ app.js
@@ -17,9 +17,10 @@ const { compile } = require('morgan');
 
 const app = express();
 //use cors to give people access
+//'https://edward-latex-react-frontend.herokuapp.com'
 app.use(
   cors({
-    origin: 'https://markdown-frontend.herokuapp.com/',
+    origin: 'https://markdown-frontend.herokuapp.com',
     credentials: true
   })
 );
